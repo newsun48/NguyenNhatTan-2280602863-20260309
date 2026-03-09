623f05c0e77a1c60564bd480d3998a70f14ceaf7
newsun48 <savanjnz2@gmail.com>
2026-03-09 15:22:59 +0700

    Initial commit with authentication and authorization

---
+++ controllers/users.js
@@ -0,0 +1,37 @@
+let userModel = require('../schemas/users')
+let bcrypt = require('bcrypt')
+module.exports = {
+    CreateAnUser: async function (username, password, email, role,
+        avatarUrl, fullName, status, loginCount
+    ) {
+        let newUser = new userModel({
+            username: username,
+            password: password,
+            email: email,
+            role: role,
+            avatarUrl: avatarUrl,
+            fullName: fullName,
+            status: status,
+            loginCount: loginCount
+        })
+        await newUser.save();
+        return newUser;
+    },
+    QueryByUserNameAndPassword: async function (username, password) {
+        let getUser = await userModel.findOne({ username: username });
+        if (!getUser) {
+            return false;
+        }
+        let isMatch = await bcrypt.compare(password, getUser.password);
+        if (isMatch) {
+            return getUser;
+        }
+        return false;
+    },
+    FindUserById: async function (id) {
+        return await userModel.findOne({
+            _id: id,
+            isDeleted:false
+        }).populate('role')
+    }
+}
